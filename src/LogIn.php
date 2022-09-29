38af7471ec8062d0bf17efe40efd601520990c76
Gaelle <g.ikuna@interface3.be>
2022-09-29 09:29:54 +0200

    gaelle c

--- src/LogIn.php
+++ src/LogIn.php
@@ -14,42 +14,6 @@ include_once './Connect_db-walk_in_Brussels.php';
 <body>
 <?php
 
-// Vérification du nom d'utilisateur
-
-$isFound = false; // connu
-
-if(isset($_POST['username'], $_POST['password'])){
-    $user_name = $_POST['username'];
-    $pass_word = $_POST['password'];
-
-
-    $sqlQuery = "
-        SELECT *
-        FROM
-        WHERE username = :param_username AND :param_password
-
-    ";
-
-    $statement = $mysqldb ->prepare($sqlQuery);
-    $statement->execute(array("param_username" => $user_name, "param_password" => $pass_word));
-    $result = $statement ->fetchAll();
-
-    if($result){
-        $db_usrName = $result[0]['username'];
-        $db_passwd = $result[0]['password'];
-
-        if($result){
-            $isFound = true;
-        }
-    }
-
-}
-
-if($isFound){
-    echo "Welcome, Start your adventure with us !";
-}else {
-    include_once('./views/login.php');
-}
 
 ?>
 </body>
