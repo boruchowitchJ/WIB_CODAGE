f1718733f93ed8a1bf9cbdbb9081402894485903
boruchowitch jennifer <boru_jennifer@hotmail.fr>
2022-09-29 16:24:09 +0200

    love

--- src/views/carte.php
+++ src/views/carte.php
@@ -1,8 +1,18 @@
 <div class="container">
-    <img src="./assets\images\background\Carte-bruxelles1.png" class='carte' alt="carte">
-    <img src="./assets/images/illu/basilique-01.png" class="picto_Atom" alt="Picto">
+    <img src="./assets\images\background\Carte-bruxelles1.png" class='carte' alt="carte">    
     <img src="./assets/images/illu/basilique-01.png" class="picto_Basi" alt="Picto">
 </div>
 
-
-<a href="?page=carte&poi=atomium">La carte SOUMAYA</a>
+<!-- pour que les picto ouvre les slides. -->
+<div class="container"><a href="?page=carte&poi=atomium"><img src="./assets/images/illu/basilique-01.png" class="picto_Atom" alt="Picto"></a> 
+<a href="?page=carte&poi=atomium"><img src="./assets/images/illu/basilique-01.png" class="picto_Atom" alt="Picto"></a> 
+<a href="?page=carte&poi=atomium"><img src="./assets/images/illu/basilique-01.png" class="picto_Atom" alt="Picto"></a> 
+<a href="?page=carte&poi=atomium"><img src="./assets/images/illu/basilique-01.png" class="picto_Atom" alt="Picto"></a> 
+<a href="?page=carte&poi=atomium"><img src="./assets/images/illu/basilique-01.png" class="picto_Atom" alt="Picto"></a> 
+<a href="?page=carte&poi=atomium"><img src="./assets/images/illu/basilique-01.png" class="picto_Atom" alt="Picto"></a> 
+<a href="?page=carte&poi=atomium"><img src="./assets/images/illu/basilique-01.png" class="picto_Atom" alt="Picto"></a> 
+<a href="?page=carte&poi=atomium"><img src="./assets/images/illu/basilique-01.png" class="picto_Atom" alt="Picto"></a> 
+<a href="?page=carte&poi=atomium"><img src="./assets/images/illu/basilique-01.png" class="picto_Atom" alt="Picto"></a> 
+<a href="?page=carte&poi=atomium"><img src="./assets/images/illu/basilique-01.png" class="picto_Atom" alt="Picto"></a> 
+<a href="?page=carte&poi=atomium"><img src="./assets/images/illu/basilique-01.png" class="picto_Atom" alt="Picto"></a> 
+</div>
