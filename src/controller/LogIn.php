d5758a4f1c6d1f8c6165f8899949fe9af4eae3bb
Gaelle <g.ikuna@interface3.be>
2022-10-03 12:21:41 +0200

    login

--- src/controller/LogIn.php
+++ src/controller/LogIn.php
@@ -1,7 +1,24 @@
-
 <?php
+include "./src/db/db.php";
+
+if(isset($_POST['email']) && isset($_POST['pswd'])){
+
+    // connexion à la base de données
+    $host = "remotemysql.com";
+    $user = "qypIEJ70Uk";
+    $db = "qypIEJ70Uk";
+    $pwd = "LTkbwXTH1g";
+    $port = 3306;
+
+
+
+}
+
+
+
+
+
 
 
 include "./src/views/login.php";
-
 ?>
--- src/views/login.php
+++ src/views/login.php
@@ -12,31 +12,31 @@
             <div class="formulaire_container">
                 <div class="formulaire">
 
-                    <form method="post" action="../controller/LogIn.php">
+                    <form method="POST" action="#">
                         <div class="formulaire_text">
                             <h1>welcome</h1>
                             <p>Sign in to your account</p>
                         </div>
 
                        <div class="inputData">
-                        
+
                        <div class="inputGroup">
-                            <input class="inputbox" type="text" name="username" id="user" placeholder="Name">
+                            <input class="inputbox" type="text" name="email" id="email" placeholder="Name">
                         </div>
 
 
                         <div class="inputGroup">
-                            <input class="inputbox" type="password" name="password" id="pswd" placeholder="password">
-                            
+                            <input class="inputbox" type="password" name="pswd" id="pswd" placeholder="password">
+
                         </div>
                         <h4>forgot your password ?</h4>
                        </div>
-                        
+
 
                         <!------- Bouton-------->
                         <div class="sign_in">
                             <h2>Log in</h2>
-                            <input class="btn_log" type="submit" value="">
+                            <input class="btn_log" type="submit" name="connexion" value="">
                         </div>
                         <div class="createAccount">
                             <p>Don't have an account ?<a href="?page=createAccount">Create</a></p>
