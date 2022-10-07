fc5084b88a61e659acb94a92edad354b5d9aabcb
Gaelle <g.ikuna@interface3.be>
2022-10-07 09:33:19 +0200

    spots

--- src/views/allSpots.php
+++ src/views/allSpots.php
@@ -1,30 +1,9 @@
-<div class="navAboutUs">
-
-    <div class="right">
-        <div class="search">
-            <label for="recherche"></label>
-            <input id="recherche" type="search" placeholder="Search">
-        </div>
-
-        <div class="langue">
-            <nav>
-                <ul>
-                    <li> |FR |</li>
-                    <li> NL |</li>
-                    <li> EN |</li>
-                </ul>
-            </nav>
-
-        </div>
-        <img src="./assets/images/illu/burger_menu.png" class="burger" alt="">
-    </div>
-</div>
 <div class="grid-container">
     <div class="grid-items">
         <div class="card">
             <div class="image-content">
                 <div class="card-image">
-                    <img src="./frederic-paulussen-xXVypHWH5WY-unsplash.jpg" alt="" class="card-img" />
+                    <img src="./assets/images/place/atomium.jpg" alt="" class="card-img" />
                 </div>
             </div>
         </div>
