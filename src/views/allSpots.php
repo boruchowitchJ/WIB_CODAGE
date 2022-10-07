a2b0ff04102cbc086a9d4bd20275939bc05419fd
Gaelle <g.ikuna@interface3.be>
2022-10-07 09:05:33 +0200

    allSpots

--- src/views/allSpots.php
+++ src/views/allSpots.php
@@ -1,30 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="./main.css" />
-    <title>Document</title>
-</head>
-<body>
-    <div class="grid-container">
-        <div class="grid-items">
-            <div class="card">
-                <div class="image-content">
-                    <div class="card-image">
-                        <img src="./frederic-paulussen-xXVypHWH5WY-unsplash.jpg" alt="" class="card-img" />
-                    </div>
+<div class="grid-container">
+    <div class="grid-items">
+        <div class="card">
+            <div class="image-content">
+                <div class="card-image">
+                    <img src="./frederic-paulussen-xXVypHWH5WY-unsplash.jpg" alt="" class="card-img" />
                 </div>
-            </div>            
+            </div>
         </div>
-        <div class="grid-items"></div>
-        <div class="grid-items"></div>
-        <div class="grid-items"></div>
-        <div class="grid-items"></div>
-        <div class="grid-items"></div>
-
-</body>
-
-</html>
+    </div>
+    <div class="grid-items"></div>
+    <div class="grid-items"></div>
+    <div class="grid-items"></div>
+    <div class="grid-items"></div>
+    <div class="grid-items"></div>
+</div>
