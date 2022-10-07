b2084498a40c38a4dac4aab8ea218d89f3f72b57
boruchowitch jennifer <boru_jennifer@hotmail.fr>
2022-10-07 09:32:37 +0200

    allSpots

--- src/views/allSpots.php
+++ src/views/allSpots.php
@@ -1,24 +1,33 @@
-<div class="navAboutUs">
-    
-    <div class="right">
-        <div class="search">
-            <label for="recherche"></label>
-            <input id="recherche" type="search" placeholder="Search">
-        </div>
+<header class="header">
+<div class="logo_header">
 
-        <div class="langue">
-            <nav>
-                <ul>
-                    <li> |FR |</li>
-                    <li> NL |</li>
-                    <li> EN |</li>
-                </ul>
-            </nav>
+<img class="img_logo" src="./assets/images/illu/Logo.png">
 
-        </div>
-        <img src="./assets/images/illu/burger_menu.png" class="burger" alt="">
-    </div>
+
+
 </div>
+    <div class="nav">
+
+        <div class="right">
+            <div class="search">
+                <label for="recherche"></label>
+                 <input id="recherche" type="search" placeholder="Search">
+            </div>
+
+            <div class="langue">
+                <nav>
+                    <ul>
+                        <li> |FR |</li>
+                        <li> NL |</li>
+                        <li> EN |</li>
+                    </ul>
+                </nav>
+            </div>
+            <img src="./assets/images/illu/burger_menu.png" class="burger" alt="">
+        </div>
+    </div>
+
+</header>
 <div class="grid-container">
     <div class="grid-items">
         <div class="card">
