b088c476370484dfd501f92ce39dd218f6a82828
Gaelle <g.ikuna@interface3.be>
2022-10-07 13:55:55 +0200

    allspotsCorection

--- src/views/allSpots.php
+++ src/views/allSpots.php
@@ -1,25 +1,3 @@
-<header class="header">
-    <div class="logo_header">
-        <img class="img_logo" src="./assets/images/illu/Logo.png">
-    </div>
-    <div class="nav">
-        <div class="right">
-            <div class="search">
-                <label for="recherche"></label>
-                <input id="recherche" type="search" placeholder="Search">
-            </div>
-            <div class="langue">
-                <nav>
-                    <ul>
-                        <li> |FR |</li>
-                        <li> NL |</li>
-                        <li> EN |</li>
-                    </ul>
-                </nav>
-
-    </div> -->
-
-</header>
 <div class="title-spots">
     <h1 class="text-spots">All spots</h1>
     <div class="divider"></div>
