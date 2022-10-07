21b15fedffff1cdaa99e6ab8f3e74bdb0e3abc01
Gaelle <g.ikuna@interface3.be>
2022-10-07 09:17:53 +0200

    spots

--- src/views/allSpots.php
+++ src/views/allSpots.php
@@ -1,3 +1,24 @@
+<div class="navAboutUs">
+
+    <div class="right">
+        <div class="search">
+            <label for="recherche"></label>
+            <input id="recherche" type="search" placeholder="Search">
+        </div>
+
+        <div class="langue">
+            <nav>
+                <ul>
+                    <li> |FR |</li>
+                    <li> NL |</li>
+                    <li> EN |</li>
+                </ul>
+            </nav>
+
+        </div>
+        <img src="./assets/images/illu/burger_menu.png" class="burger" alt="">
+    </div>
+</div>
 <div class="grid-container">
     <div class="grid-items">
         <div class="card">
